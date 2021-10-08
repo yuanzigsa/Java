aa92d69ed164508a4c42cd4579951bcdef3200f9
Yuanzi <30451380+yuanzigsa@users.noreply.github.com>
2021-10-08 17:34:50 +0800

    Revert "补录"
    This reverts commit 01ee32d481b4868a69ecd1b05935579397fb5aa9.

--- VariableTest.java
+++
@@ -1,6 +0,0 @@
-class VariableTest{
-	public static void main(String[] args){
-		int myAge = 23;
-		System.out.println(myAge);
-		}
-}
--- src/chapter6/Car.java
+++
@@ -1,8 +0,0 @@
-package chapter6;
-
-public class Car {
-    int speed = 50;
-    String color = "白色";
-    String name = "无名汽车";
-    String direction = "北方";
-}
