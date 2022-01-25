00a3cf350d51ab69aad23d2c337c380a895de6cf
yuanzigsa <yuanzigsa@gmail.com>
2022-01-25 23:43:56 +0800

    Create Video.java

---
+++ MultiThreading/Video.java
@@ -0,0 +1,16 @@
+package MultiThreading;
+
+public class Video {
+    private String name;
+    public Video(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
